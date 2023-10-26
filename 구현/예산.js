1be4c0a5b29845dadeee5efb344785f4db3e038d
goddls123 <61724496+goddls123@users.noreply.github.com>
2023-10-26 09:35:04 +0900

    Create 예산.js

---
+++ 구현/예산.js
@@ -0,0 +1,13 @@
+//프로그래머스 레벨 1
+
+function solution(d, budget) {
+    var answer = 0;
+    d.sort((a,b)=>a-b)
+    for(let i=0;i<d.length;i++){
+        budget -= d[i]
+        
+        if(budget<0)break
+        answer++
+    }
+    return answer;
+}
