154fe72ac601af06460048d61f85ff7bb8a45a34
goddls123 <61724496+goddls123@users.noreply.github.com>
2023-10-27 15:54:08 +0900

    Create 예상 대진표.js

---
+++ 구현/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    var answer = 0;
+
+    while(a!==b){
+        a= Math.floor((a+1)/2)
+        b= Math.floor((b+1)/2)
+        answer++
+    }
+
+    return answer;
+}
