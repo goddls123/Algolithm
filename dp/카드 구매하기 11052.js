4dcfc1995b77de905fb39209429240bf19ffc4c1
goddls123 <61724496+goddls123@users.noreply.github.com>
2023-11-09 08:52:05 +0900

    Create 카드 구매하기 11052.js

---
+++ dp/카드 구매하기 11052.js
@@ -0,0 +1,15 @@
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
+
+const N = Number(input.shift());
+
+const cards = input[0].split(" ").map(Number);
+
+const dp = new Array(N + 1).fill(0);
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= i; j++) {
+    dp[i] = Math.max(dp[i], dp[i - j] + cards[j - 1]);
+  }
+}
+console.log(dp[N]);
