47136aeee971c76d6a04a5b1a83492070abd96cd
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-01-18 11:02:18 +0900

    [PO] 모의고사 1회 3번 (1번)

---
+++ PCCP/유전법칙.js
@@ -0,0 +1,24 @@
+function solution(queries) {
+    var answer = [];
+    const gen = ['RR','Rr','Rr','rr']
+    const recursion = (L,m)=>{
+        if(L===2){
+            return gen[m]
+        }
+        const parent = recursion(L-1, Math.floor(m/4))
+        
+        if(parent ==='Rr'){
+            return gen[m%4]
+        }else {
+            return parent
+        }
+    }
+    answer = queries.map(([n,p])=>{
+        if(n===1){
+            return 'Rr'
+        }else{
+           return recursion(n,p-1)
+        }
+    })
+    return answer;
+}
