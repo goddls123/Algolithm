1c0254a72a5f561808d50ffb4cb663ef1dd7c6b0
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-01-26 09:15:23 +0900

    [BOJ] 아기상어 골드3 (2번)

--- BFS/16236.js
+++ BFS/16236.js
@@ -4,77 +4,73 @@ let input = require("fs").readFileSync(file).toString().trim().split("\n");
 const N = Number(input.shift());
 const board = input.map((i) => i.split(" ").map(Number));
 
+let shark = [];
 let level = 2;
 let status = 0;
-const fishes = [];
-let shark = [];
 
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
     if (board[i][j] === 9) {
-      board[i][j] = 0;
-      shark = [i, j];
-    } else if (board[i][j] === 0) {
-      continue;
-    } else {
-      fishes.push([0, i, j, board[i][j]]);
+      shark.push(i, j);
     }
   }
 }
 
-const next = (start) => {
-  const dx = [0, -1, 0, 1];
-  const dy = [-1, 0, 1, 0];
-  const queue = [start];
+const bfs = (start) => {
+  let queue = [start];
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, -1, 0, 1];
   const visited = Array.from(new Array(N), () => new Array(N).fill(-1));
   visited[start[0]][start[1]] = 0;
 
   while (queue.length) {
-    const [x, y] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      const ax = dx[i] + x;
-      const ay = dy[i] + y;
-      if (ax >= 0 && ay >= 0 && ax < N && ay < N) {
-        if (visited[ax][ay] === -1 && board[ax][ay] <= level) {
-          visited[ax][ay] = visited[x][y] + 1;
-          queue.push([ax, ay]);
+    let nq = [];
+    let tmp = [];
+
+    for (let i = 0; i < queue.length; i++) {
+      const [x, y] = queue[i];
+      for (let j = 0; j < 4; j++) {
+        const ax = dx[j] + x;
+        const ay = dy[j] + y;
+
+        if (ax >= 0 && ay >= 0 && ax < N && ay < N) {
+          if (visited[ax][ay] === -1 && board[ax][ay] <= level) {
+            visited[ax][ay] = visited[x][y] + 1;
+            if (board[ax][ay] && board[ax][ay] < level) {
+              tmp.push([ax, ay]);
+            } else {
+              nq.push([ax, ay]);
+            }
+          }
         }
       }
     }
-  }
+    if (tmp.length) {
+      tmp.sort((a, b) => (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
 
-  for (let i = 0; i < fishes.length; i++) {
-    const [d, x, y, s] = fishes[i];
-    fishes[i][0] = visited[x][y];
-  }
-
-  fishes.sort((a, b) => {
-    if (a[0] === b[0]) {
-      if (a[1] === b[1]) {
-        return a[2] - b[2];
+      const [x, y] = tmp[0];
+      status++;
+      if (level === status) {
+        level++;
+        status = 0;
       }
-      return a[1] - b[1];
+      board[x][y] = 0;
+      shark = [x, y];
+      return visited[x][y];
     }
-    return a[0] - b[0];
-  });
-
-  let index = fishes.findIndex((f) => f[0] !== -1 && f[3] < level);
-
-  return index >= 0 ? fishes.splice(index, 1).pop() : [-1, 0, 0, 0];
-};
-let t = 0;
-while (fishes.length) {
-  const [d, x, y, s] = next(shark);
-
-  if (d === -1) break;
-
-  status++;
-  if (status === level) {
-    level++;
-    status = 0;
+    queue = nq;
   }
-  shark = [x, y];
-  t += d;
-}
 
-console.log(t);
+  return 0;
+};
+
+let time = 0;
+
+while (1) {
+  const d = bfs(shark);
+
+  if (d === 0) break;
+
+  time += d;
+}
+console.log(time);
