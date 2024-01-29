c5c4aa8ca8ee901ae3580acc79b0ac835bc665a7
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-01-29 12:04:50 +0900

    [PO] PCCP 모의고사 1회 3번 (1번)

---
+++ PCCP/카페 확장.js
@@ -0,0 +1,25 @@
+function solution(menu, order, k) {
+    var answer = 0;
+    let queue = [menu[order[0]]]
+    
+    for(let i=1;i<order.length;i++){
+       
+        let time =k
+        let j =0
+        while(time){
+            if(queue[0]>=time){
+                queue[0]-=time
+                if(queue[0]===0)queue.shift()
+                break
+            }else{
+                time-=queue[0]
+                queue.shift()
+            }
+        }
+        // queue = queue.filter(q=>q>0)
+        queue.push(menu[order[i]])
+        answer =Math.max(answer,queue.length)
+    }
+    
+    return answer;
+}
