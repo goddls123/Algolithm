028d10e450ceadc800edfa94b7281a815bc35480
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-03 17:04:32 +0900

    [BOJ] 토마토 골드5 (3번)

--- BFS/토마토 7576.js
+++ BFS/토마토 7576.js
@@ -1,29 +1,38 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs")
-  .readFileSync(file)
-  .toString()
-  .trim()
-  .split("\n")
-  .map((i) => i.split(" ").map(Number));
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
-const [M, N] = input.shift();
-const maps = input;
+const [M, N] = input.shift().split(" ").map(Number);
+const board = input.map((i) => i.split(" ").map(Number));
+const visited = Array.from(new Array(N), () => new Array(M).fill(false));
+const dx = [-1, 0, 1, 0];
+const dy = [0, -1, 0, 1];
+let tomatos = [];
 
-let list = [];
+let count = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (board[i][j] === 1) {
+      tomatos.push([i, j]);
+    } else if (board[i][j] === 0) {
+      count++;
+    }
+  }
+}
 
-const BFS = (queue) => {
+const dfs = (queue) => {
   const tmp = [];
-  const dx = [0, -1, 0, 1];
-  const dy = [-1, 0, 1, 0];
+
   for (let i = 0; i < queue.length; i++) {
     const [x, y] = queue[i];
+
+    visited[x][y] = true;
     for (let j = 0; j < 4; j++) {
       const ax = x + dx[j];
       const ay = y + dy[j];
-
       if (ax >= 0 && ay >= 0 && ax < N && ay < M) {
-        if (maps[ax][ay] === 0) {
-          maps[ax][ay] = 1;
+        if (!visited[ax][ay] && !board[ax][ay]) {
+          visited[ax][ay] = true;
+          count--;
           tmp.push([ax, ay]);
         }
       }
@@ -31,30 +40,11 @@ const BFS = (queue) => {
   }
   return tmp;
 };
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    if (maps[i][j] === 1) {
-      list.push([i, j]);
-    }
-  }
+let time = 0;
+while (1) {
+  tomatos = dfs(tomatos);
+  if (!tomatos.length) break;
+  time++;
 }
 
-let count = 0;
-
-while (list.length) {
-  list = BFS(list);
-  if (list.length > 0) {
-    count++;
-  }
-}
-
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    if (maps[i][j] === 0) {
-      console.log(-1);
-      return;
-    }
-  }
-}
-
-console.log(count);
+console.log(count === 0 ? time : -1);
