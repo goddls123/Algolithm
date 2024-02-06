22b84b5cff71bf962ac77abcb6df9118a05415e8
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-06 17:29:57 +0900

    [BOJ] 쿼드트리 실버1 (1번)

--- backtracking/1992.js
+++ backtracking/1992.js
@@ -1,10 +1,11 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs").readFileSync(file).toString().split("\n");
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
 const N = Number(input.shift());
 
 const board = input.map((i) => i.split("").map(Number));
-function recursion(x, y, n) {
+
+const recursion = (x, y, n) => {
   if (n === 1) {
     return board[x][y];
   }
@@ -14,16 +15,13 @@ function recursion(x, y, n) {
   const tmp3 = recursion(x + n, y, n);
   const tmp4 = recursion(x + n, y + n, n);
 
-  if (
-    (tmp1 === 1 || tmp1 === 0) &&
-    tmp1 === tmp2 &&
-    tmp1 === tmp3 &&
-    tmp1 === tmp4
-  ) {
-    return tmp1;
+  if (tmp1 + tmp2 + tmp3 + tmp4 === 0) {
+    return 0;
+  } else if (tmp1 + tmp2 + tmp3 + tmp4 === 4) {
+    return 1;
   } else {
     return `(${tmp1}${tmp2}${tmp3}${tmp4})`;
   }
-}
-
-console.log(recursion(0, 0, N));
+};
+const result = recursion(0, 0, N);
+console.log(result);
