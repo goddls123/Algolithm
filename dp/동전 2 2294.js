a4ee82df366bb2178cc1118c1373241ee134d7c6
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-11 07:36:09 +0900

    [BOJ] 동전 2 골드5 (5번)

--- dp/동전 2 2294.js
+++ dp/동전 2 2294.js
@@ -1,19 +1,18 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
-const [N, M] = input.shift().split(" ").map(Number);
+const [N, K] = input.shift().split(" ").map(Number);
+const coins = input.map(Number);
 
-const coins = Array.from(new Set([...input.map(Number)]));
-const size = coins.length;
-const dp = new Array(M + 1).fill(Infinity);
-dp[0] = 0;
+const money = new Array(K + 1).fill(Infinity);
 
-for (let i = 1; i <= M; i++) {
-  for (let j = 0; j < size; j++) {
+money[0] = 0;
+
+for (let i = 1; i <= K; i++) {
+  for (let j = 0; j < N; j++) {
     if (i - coins[j] < 0) continue;
-
-    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+    money[i] = Math.min(money[i], money[i - coins[j]] + 1);
   }
 }
 
-console.log(dp[M] === Infinity ? -1 : dp[M]);
+console.log(money[K] === Infinity ? -1 : money[K]);
