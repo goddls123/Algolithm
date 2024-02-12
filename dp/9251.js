ce1bb2ebc622e04400a186fea64fcb9e03f751c9
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-13 06:28:08 +0900

    [BOJ] LCS 골드5 (5번)

--- dp/9251.js
+++ dp/9251.js
@@ -1,15 +1,16 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs").readFileSync(file).toString().trim().split("\n");
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
 const str1 = input[0];
 const str2 = input[1];
-const n = str1.length;
-const m = str2.length;
 
-const dp = Array.from(new Array(n + 1), () => new Array(m + 1).fill(0));
+const N = str1.length;
+const M = str2.length;
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= m; j++) {
+const dp = Array.from(new Array(N + 1), () => new Array(M + 1).fill(0));
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= M; j++) {
     if (str1[i - 1] === str2[j - 1]) {
       dp[i][j] = dp[i - 1][j - 1] + 1;
     } else {
@@ -17,4 +18,5 @@ for (let i = 1; i <= n; i++) {
     }
   }
 }
-console.log(dp[n][m]);
+
+console.log(dp[N][M]);
