f475b7fd79acb6e5850644f35106604e87e5f51b
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-13 08:00:44 +0900

    [BOJ] 지름길 실버1 (2번)

--- gready/1446.js
+++ gready/1446.js
@@ -1,33 +1,31 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs")
-  .readFileSync(file)
-  .toString()
-  .trim()
-  .split("\n")
-  .map((i) => i.split(" ").map(Number));
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
-const [N, D] = input.shift();
+const [N, D] = input.shift().split(" ").map(Number);
+
 const dp = new Array(D + 1).fill(Infinity);
-const short = new Map();
+const map = new Map();
 
 for (let i = 0; i < N; i++) {
-  const [from, to, cost] = input[i];
-
-  if (to > D || cost >= to - from) continue;
-  if (short.has(to)) {
-    short.get(to).push([cost, from]);
+  const [start, end, cost] = input[i].split(" ").map(Number);
+  if (end > D) continue;
+  if (map.has(end)) {
+    map.get(end).push([start, cost]);
   } else {
-    short.set(to, [[cost, from]]);
+    map.set(end, [[start, cost]]);
   }
 }
-
 dp[0] = 0;
 for (let i = 1; i <= D; i++) {
   dp[i] = dp[i - 1] + 1;
-  if (short.has(i)) {
-    short.get(i).forEach(([cost, from]) => {
-      dp[i] = Math.min(dp[i], dp[from] + cost);
-    });
+  if (map.has(i)) {
+    const route = map.get(i);
+    for (let j = 0; j < route.length; j++) {
+      const [start, cost] = route[j];
+
+      dp[i] = Math.min(dp[start] + cost, dp[i]);
+    }
   }
 }
+
 console.log(dp[D]);
