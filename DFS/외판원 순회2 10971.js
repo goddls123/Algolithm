dc4cf8bedd9aad80c43bb83c8305a75588d9fb05
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-12 11:04:40 +0900

    [BOJ] 외판원 순회2 실버1 (4번)

--- DFS/외판원 순회2 10971.js
+++ DFS/외판원 순회2 10971.js
@@ -3,25 +3,31 @@ const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
 const N = Number(input.shift());
 
-const graph = input.map((i) => i.split(" ").map(Number));
-const visited = new Array(N).fill(false);
+const board = input.map((i) => i.split(" ").map(Number));
+
+const visited = new Array(N).fill(0);
+const minV = Math.min(...[].concat(...board));
 
 let min = Infinity;
-
-function DFS(L, v, sum) {
-  if (L === N - 1 && graph[v][0] !== 0) {
-    min = Math.min(sum + graph[v][0], min);
+const dfs = (L, next, sum) => {
+  if (L === N - 1) {
+    if (board[next][0]) {
+      min = Math.min(min, sum + board[next][0]);
+    }
     return;
   }
+  if (sum + (L - 1) * minV > min) return;
+
   for (let i = 0; i < N; i++) {
-    if (!visited[i] && graph[v][i] !== 0) {
+    if (!visited[i] && board[next][i]) {
       visited[i] = true;
-      DFS(L + 1, i, sum + graph[v][i]);
+      dfs(L + 1, i, sum + board[next][i]);
       visited[i] = false;
     }
   }
-}
+};
 
 visited[0] = true;
-DFS(0, 0, 0);
+dfs(0, 0, 0);
+
 console.log(min);
