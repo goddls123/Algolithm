d6948c77da5ddeecabfc64f9589eec2b484047e2
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-13 10:01:53 +0900

    [BOJ] 녹색 옷 입은 애가 젤다지? 골드4 (3번)

--- BFS/4485.js
+++ BFS/4485.js
@@ -1,22 +1,21 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs").readFileSync(file).toString().trim().split("\n");
-
-const result = [];
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
 let i = 0;
-const dx = [0, -1, 0, 1];
-const dy = [-1, 0, 1, 0];
+const result = [];
+const dx = [-1, 0, 1, 0];
+const dy = [0, -1, 0, 1];
 
-function bfs(board, n) {
+const bfs = (n, board) => {
   const queue = [[0, 0]];
-  const visited = Array.from(new Array(n), () => new Array(n).fill(Infinity));
+  let visited = Array.from(new Array(n), () => new Array(n).fill(Infinity));
   visited[0][0] = board[0][0];
   while (queue.length) {
     const [x, y] = queue.shift();
 
     for (let i = 0; i < 4; i++) {
-      const ax = x + dx[i];
-      const ay = y + dy[i];
+      const ax = dx[i] + x;
+      const ay = dy[i] + y;
       if (ax >= 0 && ay >= 0 && ax < n && ay < n) {
         if (visited[ax][ay] > visited[x][y] + board[ax][ay]) {
           visited[ax][ay] = visited[x][y] + board[ax][ay];
@@ -26,17 +25,18 @@ function bfs(board, n) {
     }
   }
   return visited[n - 1][n - 1];
-}
+};
+
+let k = 1;
 while (1) {
-  const n = Number(input[i++]);
-  if (n === 0) break;
-  const board = Array.from(new Array(n), () => []);
-
-  for (let j = 0; j < n; j++) {
-    board[j] = input[i + j].split(" ").map(Number);
+  const N = Number(input[i++]);
+  if (N === 0) break;
+  const board = [];
+  for (let j = 0; j < N; j++) {
+    board.push(input[i++].split(" ").map(Number));
   }
-  result.push(bfs(board, n));
-  i += n;
+  result.push(`Problem ${k}: ${bfs(N, board)}`);
+  k++;
 }
 
-console.log(result.map((r, i) => `Problem ${i + 1}: ${r}`).join("\n"));
+console.log(result.join("\n"));
