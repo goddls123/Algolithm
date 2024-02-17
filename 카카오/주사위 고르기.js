8f7b074c3b18c8ce473254a639e3e3ec800d3e4c
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-17 17:20:48 +0900

    [PO] 2024 WINTER INTERN Lv3 (1번)

---
+++ 카카오/주사위 고르기.js
@@ -0,0 +1,79 @@
+function solution(dice) {
+    var answer = [];
+    let max =0
+    const n = dice.length
+    const array = new Array(n).fill(0).map((a,i)=>i)
+    const list = getComb(n/2,array)
+    list.forEach(dice1=>{
+        const used = new Array(n).fill(0)
+        for(let i=0;i<dice1.length;i++){
+            used[dice1[i]] = true
+        }
+        const dice2 = []
+        for(let i=0;i<used.length;i++){
+            if(!used[i]){
+                dice2.push(i)
+            }
+        }
+        
+        const sum1 = getSumList(dice,dice1)
+        const sum2 = getSumList(dice,dice2).sort((a,b)=>a-b)
+        let count =0
+        for(let i=0;i<sum1.length;i++){
+            const tmp = getVictoryCount(sum1[i],sum2)
+            count += tmp
+        }
+        if(count>max){
+            answer = dice1
+            max =count
+        }
+    })
+    
+    return answer.map(a=>a+1);
+}
+
+function getVictoryCount(n,list){
+    let left = 0
+    let right = list.length-1
+    if(n>list[right]){
+        return right+1
+    }
+    
+    while(left<right){
+        let mid = Math.floor((left+right)/2)
+        
+        if(list[mid]<n){
+            left = mid+1
+        }else{
+            right=mid
+        }
+    }
+    return right
+}
+function getComb(L,array){
+    if(L ===1){
+        return array.map(a=>[a])
+    }
+    const result=[]
+    for(let i=0;i<array.length;i++){
+        const rest = array.slice(i+1)
+        const comb = getComb(L-1,rest)
+        const attach = comb.map(c=>[array[i],...c])
+        result.push(...attach)
+    }
+    return result
+}
+function getSumList (dice, list){
+   let tmp = [...dice[list[0]]]
+   for(let i=1;i<list.length;i++){
+       const tmp1 =[]
+       for(let j=0;j<tmp.length;j++){
+           for(let k=0;k<6;k++){
+               tmp1.push(dice[list[i]][k]+ tmp[j])
+           }
+       }
+       tmp = tmp1
+   }
+    return tmp
+}
+
