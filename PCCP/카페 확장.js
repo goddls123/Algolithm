87c0696237138e398106487748815c99efb921ab
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-21 16:15:49 +0900

    [PO] PCCP 모의고사 2회 3번 (2번)

--- PCCP/카페 확장.js
+++ PCCP/카페 확장.js
@@ -1,24 +1,50 @@
+class Node {
+    constructor(value){
+        this.value =value
+        this.next = null
+    }
+}
+class Queue {
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length=0
+    }
+    push(v){
+        const tmp = new Node(v)
+
+        if(this.length===0){
+            this.head = tmp
+        }else{
+            this.tail.next = tmp
+        }
+        this.tail =tmp
+        this.length++
+    }
+    shift(){
+        if(this.length===0) return
+        this.head = this.head.next
+        this.length--
+    }
+}
 function solution(menu, order, k) {
-    var answer = 0;
-    let queue = [menu[order[0]]]
-    
+    let max = 0
+    const queue =new Queue()
+    queue.push(menu[order[0]])
     for(let i=1;i<order.length;i++){
-       
-        let time =k
-        let j =0
-        while(time){
-            if(queue[0]>=time){
-                queue[0]-=time
-                if(queue[0]===0)queue.shift()
+        let time = k
+        while(time!==0 ){
+            if(queue.head===null) break
+
+            queue.head.value -= time
+            if(queue.head.value>0){
                 break
-            }else{
-                time-=queue[0]
-                queue.shift()
             }
+            time = Math.abs(queue.head.value)
+            queue.shift() 
         }
         queue.push(menu[order[i]])
-        answer =Math.max(answer,queue.length)
+        max = Math.max(max,queue.length)
     }
-    
-    return answer;
+    return max;
 }
