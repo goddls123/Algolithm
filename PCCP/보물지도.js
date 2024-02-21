44751f7dcf76f83361cfd6407a379deb12ae5937
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-21 16:03:30 +0900

    [PO] PCCP 모의고사 2회 4번 (2번)

--- PCCP/보물지도.js
+++ PCCP/보물지도.js
@@ -1,53 +1,51 @@
 function solution(n, m, hole) {
-    var answer = -1
-    let board = Array.from(new Array(n),()=>new Array(m).fill(1))
-    const visited = Array.from(new Array(n),()=>Array.from(new Array(m),()=>new Array(2).fill(0))) 
-    const dx =[0,-1,0,1]
-    const dy =[-1,0,1,0]
-    hole.forEach(([x,y])=>{
-        board[x-1][y-1]=0
+    const board = Array.from(new Array(m), ()=> new Array(n).fill(0))
+
+    hole.forEach(([y,x])=>{
+        board[x-1][y-1] =2
     })
-    
+
     const bfs =()=>{
-        const queue =[[0,0,0,0]]
+        const dx =[0,-1,0,1]
+        const dy = [-1,0,1,0]
+        const queue = [[0,0,0,0]]
+        const visited = Array.from(new Array(m), ()=> Array.from( new Array(n),()=>new Array(2).fill(0)))
         visited[0][0][0]=true
-        
+
         while(queue.length){
-            const [x,y,flag,count]= queue.shift()
-            
-            if(x===n-1 && y===m-1){
+            const [x,y,flag,count] =queue.shift()
+            if(x===m-1 && y===n-1){
                 return count
             }
-            
             for(let i=0;i<4;i++){
                 const ax = dx[i]+x
                 const ay = dy[i]+y
-                if(ax>=0 && ay>=0 && ax<n && ay<m){
-                    if(!visited[ax][ay][flag] && board[ax][ay]){
+
+                if(ax>=0&&ay>=0 &&ax<m && ay<n){
+                    if(!visited[ax][ay][flag] && !board[ax][ay]){
                         visited[ax][ay][flag]=true
                         queue.push([ax,ay,flag,count+1])
                     }
                 }
             }
-            if(flag) continue
-            
-            for(let i=0;i<4;i++){
-                const ax = dx[i]*2+x
-                const ay = dy[i]*2+y
-                if(ax>=0 && ay>=0 && ax<n && ay<m){
-                    if(!visited[ax][ay][!flag] && board[ax][ay]){
-                        visited[ax][ay][!flag]=true
-                        queue.push([ax,ay,!flag,count+1])
+
+            if(!flag){
+                for(let i=0;i<4;i++){
+                    const ax = dx[i]*2+x
+                    const ay = dy[i]*2+y
+
+                    if(ax>=0&&ay>=0 &&ax<m && ay<n){
+                        if(!visited[ax][ay][!flag] && !board[ax][ay]){
+                            visited[ax][ay][!flag]=true
+                            queue.push([ax,ay,!flag,count+1])
+                        }
                     }
                 }
             }
-            
         }
-        
+
         return -1
-        
     }
-    answer = bfs()
-    
-    return answer;
+
+    return bfs();
 }
