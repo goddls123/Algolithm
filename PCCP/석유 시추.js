1f278687e75b5d5460eea46e1372a4000154c8c8
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-27 19:58:45 +0900

    [PO] PCCP 모의고사 3회 2번 (2번)

--- PCCP/석유 시추.js
+++ PCCP/석유 시추.js
@@ -1,53 +1,55 @@
 function solution(land) {
     var answer = 0;
-    let max =0
-    const list = [0,0]
     let index =2
     const n = land.length
     const m = land[0].length
+    const array = [0,0]
+    
+    const bfs = (start,flag)=>{
+        const dx = [0,-1,0,1]
+        const dy = [-1,0,1,0]    
+        const queue =[start]
+        let count =1
+        land[start[0]][start[1]] = flag
+        while(queue.length){
+            const [x,y]= queue.shift()
+            
+            for(let i=0;i<4;i++){
+                const ax = dx[i]+x
+                const ay = dy[i]+y
+               
+                if(ax>=0&&ay>=0&&ax<n&&ay<m){
+                   
+                    if(land[ax][ay]===1){
+                        count++
+                        land[ax][ay]=flag
+                        queue.push([ax,ay])
+                    }
+                }
+            }
+        }
+            return count
+    }
+    
     
     for(let i=0;i<n;i++){
         for(let j=0;j<m;j++){
             if(land[i][j]===1){
-                let count = dfs([i,j],index,n,m,land)
-                list[index++]=count
+                array.push(bfs([i,j],index))
+                index++
             }
         }
     }
-
-    for(let j=0;j<m;j++){
+    let max =0
+    for(let i=0;i<m;i++){
         const set = new Set()
-        for(let i=0;i<n;i++){
-            set.add(land[i][j])
+        for(let j=0;j<n;j++){
+            set.add(land[j][i])
         }
-        let sum = Array.from(set).reduce((a,c)=> a+ list[c],0 )
-        max = Math.max(max,sum)
-    }
-    return max;
-}
-
-function dfs (start,flag,n,m,board){
-    const queue = [start]
-    const dx =[0,-1,0,1]
-    const dy =[-1,0,1,0]
-    
-    board[start[0]][start[1]]=flag
-    let count =1
-    while(queue.length){
-        const [x,y]=queue.shift()
+        const count=Array.from(set).reduce((a,c)=>a+array[c],0)
         
-        for(let i=0;i<4;i++){
-            const ax = x+dx[i]
-            const ay = y+dy[i]
-            
-            if(ax>=0 && ay>=0 &&ax<n &&ay<m){
-                if(board[ax][ay]===1){
-                    board[ax][ay]=flag
-                    count++
-                    queue.push([ax,ay])
-                }
-            }
-        }
+        max = Math.max(max,count)
     }
-    return count
+    
+    return max;
 }
