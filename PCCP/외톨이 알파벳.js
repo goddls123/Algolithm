222387b9704d2c268f14df0876880d5acbebb888
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-27 21:07:08 +0900

    [PO] PCCP 모의고사 1회 1번 (2번)

--- PCCP/외톨이 알파벳.js
+++ PCCP/외톨이 알파벳.js
@@ -1,19 +1,20 @@
 function solution(input_string) {
     var answer = '';
-    const alphabet={a:0,b:0,c:0,d:0,e:0,f:0,g:0,h:0,i:0,j:0,k:0,l:0,m:0,n:0,o:0,p:0,q:0,r:0,s:0,t:0,u:0,v:0,w:0,x:0,y:0,z:0}
-    let end=''
-    const stack =[]
-    for(let i=0;i<input_string.length;i++){
-        if(end !==input_string[i]){
+    const alphabet = {a:0,b:0,c:0,d:0,e:0,f:0,g:0,h:0,i:0,j:0,k:0,l:0,m:0,n:0,o:0,p:0,q:0,r:0,s:0,t:0,u:0,v:0,w:0,x:0,y:0,z:0}
+    
+    let end = input_string[0]
+    alphabet[input_string[0]]++
+    
+    for(let i=1;i<input_string.length;i++){
+        if(end!==input_string[i]){
             end = input_string[i]
-            alphabet[end]++
+            alphabet[input_string[i]]++
         }
     }
-   
-    for(let [key,value] of Object.entries(alphabet)){
-        if(value>1){
+    for(const key in alphabet){
+        if(alphabet[key]>=2){
             answer+=key
         }
     }
-    return answer==='' ? 'N':answer;
+    return answer==='' ? 'N': answer;
 }
