b5d90063bd70f43576ee9da99f9205eb13e33a9f
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-01 08:27:25 +0900

    [PO] 2024 WINTER INTERN Lv2 (2번)

--- 카카오/도넛과 막대 그래프.js
+++ 카카오/도넛과 막대 그래프.js
@@ -1,45 +1,35 @@
 function solution(edges) {
-    var answer = new Array(4).fill(0);
-    const graph = new Map()
-
+    let answer = new Array(4).fill(0)
+    const map = new Map()
     
     edges.forEach(([from,to])=>{
-        if(!graph.has(from)){
-            graph.set(from,[0,0])
+        if(map.has(from)){
+            map.get(from)[0]++
         }
-        if(!graph.has(to)){
-            graph.set(to,[0,0])
+        else{
+            map.set(from,[1,0])
         }
-        graph.get(from)[0]++
-        graph.get(to)[1]++
-    })
-    
-    let total =0
-    for(let key of graph.keys()){
-        if(graph.get(key)[0] >=2 && graph.get(key)[1]===0){
-            answer[0] =key
-            total = graph.get(key)[0]
-            break
+        if(map.has(to)){
+            map.get(to)[1]++
         }
-    }
-    
-    edges.forEach(([from,to])=>{
-        if(from===answer[0]){
-            graph.get(to)[1]--
+        else{
+            map.set(to,[0,1])
         }
     })
-    
-    
-    for(let key of graph.keys()){
-        if(graph.get(key)[0]===0 && graph.get(key)[1]>=0){
+    for(let key of map.keys()){
+        if(map.get(key)[0]===0){
             answer[2]++
-        }
-        else if(graph.get(key)[0]==2 && graph.get(key)[1]==2){
+        }else if(map.get(key)[0]>=2 && map.get(key)[1]===0){
+            answer[0] = key
+        }else if(map.get(key)[0]===2 && map.get(key)[1]>0){
             answer[3]++
         }
     }
-    answer[1]=total - answer[2]-answer[3]
-
- 
+    answer[1] = map.get(answer[0])[0] - answer[2] - answer[3]
     return answer;
 }
+
+
+//진입 찻수 제일 높은거
+//진입 찻수가 그래프 총 갯수
+//
