d3077ba6eb4bfa80766a631d70cecbffa0337680
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-02-29 21:26:52 +0900

    [PO] 2024 WINTER INTERN Lv1 (2번)

--- 카카오/가장 많이 받은 선물.js
+++ 카카오/가장 많이 받은 선물.js
@@ -1,33 +1,42 @@
 function solution(friends, gifts) {
-    const n = friends.length
-    var answer = new Array(n).fill(0)
-    const dp = new Array(n).fill(0)
     const map = new Map()
-    const board = Array.from(new Array(n),()=>new Array(n).fill(0))
-    friends.forEach((f,i)=>{
-        map.set(f,i)
-    })
-    gifts.map(g=>g.split(' '))
-        .forEach(([from,to])=>{        
-        board[map.get(from)][map.get(to)]++
-        dp[map.get(from)]++
-        dp[map.get(to)]--
-    })
+    const n = friends.length
+    const board= Array.from(new Array(n),()=> new Array(n).fill(0))
+    const array = new Array(n).fill(0)
     
-    for(let i=0;i<n-1;i++){
+    for(let i=0;i<friends.length;i++){
+        map.set(friends[i],i)
+    }
+    gifts.forEach(gift=>{
+        const [from,to] = gift.split(' ').map(g=>map.get(g))
+        
+        board[from][to]++
+        board[to][from]--
+        array[from]++
+        array[to]--
+    })
+    const answer =new Array(n).fill(0)
+    for(let i=0;i<n-1;i++) {
         for(let j=i+1;j<n;j++){
-            if(board[i][j]>board[j][i]){
+            if(board[i][j] >board[j][i]){
                 answer[i]++
             }else if(board[i][j]<board[j][i]){
                 answer[j]++
             }else{
-                if(dp[i]>dp[j]){
+                if(array[i]>array[j]){
                     answer[i]++
-                }else if(dp[i]<dp[j]){
+                }else if(array[i]<array[j]){
                     answer[j]++
                 }
             }
         }
     }
+    
     return Math.max(...answer);
 }
+
+
+// 
+// 선물지수 
+// 준게 많으면 지수 높음
+// 1. 많이 준사람이 2. 지수 높은 사람이
