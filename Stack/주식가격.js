86457a210ee8dd84db0672a5b9e8f0d51885b68d
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-05 10:50:42 +0900

    [PO] 주식가격 Lv2 (3번)

--- Stack/주식가격.js
+++ Stack/주식가격.js
@@ -1,19 +1,22 @@
 function solution(prices) {
-    const n = prices.length
-    var answer = new Array(n).fill(0);
-    const stack =[]
+    var answer = [];
+    const stack = []
     
-    for(let i = 0 ; i< n;i++){
-        while(stack.length && stack[stack.length-1][0]>prices[i]){
-            const [t,index] = stack.pop()
-            answer[index]=i-index
+    for(let i=0;i<prices.length;i++){
+        if(stack.length){
+            while(stack.length && stack[stack.length-1][0]>prices[i]) {
+                const [price, index] = stack.pop()      
+                answer[index]= i-index
+            }
+            stack.push([prices[i],i])
+            
+        }else{
+            stack.push([prices[i],i])
         }
-        stack.push([prices[i],i])
     }
-    
     while(stack.length){
-          const [t,index] = stack.pop()
-          answer[index]=n-1-index
+        const [price, index] = stack.pop()      
+        answer[index]= prices.length-index-1
     }
     return answer;
 }
