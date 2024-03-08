72d89348f752a661af1d72b3c08b1297d9f0731b
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-08 14:39:19 +0900

    [PO] 2020 BLIND Lv3 (2번)

--- 카카오/자물쇠와 열쇠.js
+++ 카카오/자물쇠와 열쇠.js
@@ -1,52 +1,54 @@
 function solution(key, lock) {
-    var answer = true;
-    const n = lock.length
+    var answer = false;
     const m = key.length
-    const board= Array.from(new Array(n+m*2),()=>new Array(n+m*2).fill(2))
-    let left =0
+    const n = lock.length
+    const board= Array.from(new Array(n+2*m) , ()=>new Array(n+2*m).fill(2))
+    let count =0
     
     for(let i=0;i<n;i++){
         for(let j=0;j<n;j++){
-            board[m+i][m+j] = lock[i][j]
-            if(lock[i][j]==0){
-                left++
+            board[i+m][j+m]=lock[i][j]
+            if(lock[i][j]===0){
+                count++
             }
         }
     }
+    
     
     const rotate = (array)=>{
         const tmp = Array.from(new Array(m),()=>new Array(m).fill(0))
         for(let i=0;i<m;i++){
             for(let j=0;j<m;j++){
-                tmp[j][m-i-1] = array[i][j]
+                tmp[i][j] = array[j][m-i-1]
             }
         }
         return tmp
-    }
-    const check=(array,[x,y])=>{
-        let count = left
-        for(let i =0 ;i<m;i++){
+   }
+    const check=(x,y)=>{
+        let cnt = count 
+        
+        for(let i=0;i<m;i++){
             for(let j=0;j<m;j++){
-                if(board[x-m+i+1][y-m+j+1]===2)continue
-                 if (array[i][j]===1 && board[x-m+i+1][y-m+j+1]===1){
-                    return false
-                }else if(array[i][j]=== 1 && board[x-m+i+1][y-m+j+1]===0){
-                    count--
+                if(key[i][j] && board[x-m+1+i][y-m+1+j]===1) return false
+                
+                if(key[i][j] && board[x-m+1+i][y-m+1+j]===0){
+                    cnt--
                 }
             }
         }
-        return count ==0 ? true : false
+        return cnt == 0? true:false
     }
-    
+
     for(let i=0;i<4;i++){
-        key =rotate(key)
-        for(let j =m;j<n+m*2;j++){
-            for(let k=m;k<n+m*2;k++){
-                if(check(key, [j,k])){
+        key = rotate(key)
+        for(let i=m;i<n+2*m;i++){
+            for(let j=m;j<n+2*m;j++){
+                if(check(i,j)){
                     return true
                 }
             }
         }
     }
-    return false;
+    
+    return answer;
 }
