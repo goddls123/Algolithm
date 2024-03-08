8fab2e27f5729812b6453e35bbe64ae06f023b0a
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-08 16:17:32 +0900

    [PO] 2020 BLIND Lv3 (2번)

--- 카카오/기둥과 보 설치.js
+++ 카카오/기둥과 보 설치.js
@@ -1,64 +1,71 @@
 function solution(n, build_frame) {
     var answer = [];
-    const board= Array.from(new Array(n+1),()=>new Array(n+1).fill(0))
-    const maze= Array.from(new Array(n+1),()=>new Array(n+1).fill(0))
+    const gBoard = Array.from(new Array(n+1),()=>new Array(n+1).fill(0))
+    const bBoard = Array.from(new Array(n+1),()=>new Array(n+1).fill(0))
     
-    const checkC=(x,y)=>{
-        if(y===0 || board[x][y-1])return true
-        if(maze[x][y] || (x>0 &&maze[x-1][y])) return true
-        return false
-    }
- 
-        
     const checkG=(x,y)=>{
-        if(board[x][y-1] || board[x+1][y-1]) return true
-        if(x>0 && maze[x-1][y] && maze[x+1][y])return true
+        if(y===0 || gBoard[x][y-1]) return true
+        if(bBoard[x][y])return true
+        if(x>0 && bBoard[x-1][y])return true
+        
         return false
     }
-    
-   const enableDelete=()=>{
+    const checkB = (x,y)=>{
+        if(y===0 || x===n) return false
+        if(gBoard[x][y-1] || gBoard[x+1][y-1])return true
+        if(x>0 && bBoard[x-1][y] && bBoard[x+1][y])return true
+        
+        return false
+    }
+    const checkAll=()=>{
         for(let i=0;i<=n;i++){
             for(let j=0;j<=n;j++){
-                if(!board[i][j]) continue;
-                if(!checkC(i,j)) return false;
+                if(!gBoard[i][j] && !bBoard[i][j])continue
+                
+                if(gBoard[i][j]){
+                    if(!checkG(i,j)){
+                        return false
+                    }
+                }
+                if(bBoard[i][j]){
+                    if(!checkB(i,j)){
+                        return false
+                    }
+                }
             }
         }
-        for(let i=0;i<=n;i++){
-            for(let j=0;j<n;j++){
-                if(!maze[i][j]) continue;
-                if(!checkG(i,j)) return false;
-            }
-        }
-        return true;
-    }
-    
-    build_frame.forEach(([x,y,a,b])=>{
-        if(b){
-            if(a && checkG(x,y)){
-                maze[x][y]=1
-            }else if(a===0 && checkC(x,y)){
-                board[x][y]=1
+        return true
+    }    
+    build_frame.forEach(([x,y,a,flag])=>{
+        if(flag){
+            if(a){
+                if(checkB(x,y))
+                    bBoard[x][y]=1
+            }else{
+                if(checkG(x,y)){
+                    gBoard[x][y]=1
+                }
             }
         }else{
             if(a){
-                maze[x][y]=0
-                if(!enableDelete()){
-                    maze[x][y]=1
+                bBoard[x][y]=0
+                if(!checkAll()){
+                    bBoard[x][y]=1
                 }
-            }else {
-                board[x][y]=0
-                if(!enableDelete()){
-                    board[x][y]=1
+            }else{
+                gBoard[x][y]=0
+                if(!checkAll()){
+                    gBoard[x][y]=1
                 }
             }
         }
     })
-    for(let i=0;i<n+1;i++){
-        for(let j=0;j<n+1;j++){
-            if(board[i][j]){
+    for(let i=0;i<=n;i++){
+        for(let j=0;j<=n;j++){
+            if(gBoard[i][j]){
                 answer.push([i,j,0])
             }
-            if(maze[i][j]){
+            if(bBoard[i][j]){
                 answer.push([i,j,1])
             }
         }
