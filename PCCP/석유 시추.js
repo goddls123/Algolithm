e45d6ef72f0b45a5ffed36905ead671f619873c8
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-14 20:31:02 +0900

    [PO] PCCP 모의고사 3회 2번 (3번)

--- PCCP/석유 시추.js
+++ PCCP/석유 시추.js
@@ -1,55 +1,55 @@
 function solution(land) {
-    var answer = 0;
+    let array = [0,0]
     let index =2
     const n = land.length
     const m = land[0].length
-    const array = [0,0]
-    
-    const bfs = (start,flag)=>{
-        const dx = [0,-1,0,1]
-        const dy = [-1,0,1,0]    
-        const queue =[start]
-        let count =1
+
+    const dx =[0,-1,0,1]
+    const dy =[-1,0,1,0]
+
+    const bfs=(start,flag)=>{
+        const queue = [start]
         land[start[0]][start[1]] = flag
+        let count = 1
+
         while(queue.length){
-            const [x,y]= queue.shift()
-            
+            const [x,y]=queue.shift()
+
             for(let i=0;i<4;i++){
                 const ax = dx[i]+x
-                const ay = dy[i]+y
-               
+                const ay =dy[i]+y
+
                 if(ax>=0&&ay>=0&&ax<n&&ay<m){
-                   
                     if(land[ax][ay]===1){
-                        count++
                         land[ax][ay]=flag
+                        count++
                         queue.push([ax,ay])
                     }
                 }
             }
         }
-            return count
+        return count
     }
-    
-    
+
     for(let i=0;i<n;i++){
         for(let j=0;j<m;j++){
             if(land[i][j]===1){
-                array.push(bfs([i,j],index))
-                index++
+                const count = bfs([i,j],index)
+                array[index++]= count
             }
         }
     }
     let max =0
-    for(let i=0;i<m;i++){
-        const set = new Set()
-        for(let j=0;j<n;j++){
-            set.add(land[j][i])
+    for(let j=0;j<m;j++){
+        const set =new Set()
+        for(let i=0;i<n;i++){
+            set.add(land[i][j])
         }
-        const count=Array.from(set).reduce((a,c)=>a+array[c],0)
-        
-        max = Math.max(max,count)
+        let count =0
+        set.forEach(a=>{
+            count+=array[a]
+        })
+        max = Math.max(count,max)
     }
-    
     return max;
 }
