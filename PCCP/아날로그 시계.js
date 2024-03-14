0584bdbf276cd10bfc9e8f4a733e89cc8be7b29a
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-03-14 20:32:41 +0900

    [PO] PCCP 모의고사 3회 3번 (2번)

--- PCCP/아날로그 시계.js
+++ PCCP/아날로그 시계.js
@@ -1,23 +1,27 @@
 function solution(h1, m1, s1, h2, m2, s2) {
-    var answer = -1;
-    
-    const getCount=(h,m,s)=>{
-        const [hDegree,mDegree,sDegree]=[(h*30+0.5*m+s/120)%360,6*m+0.1*s,6*s]
-        let count = -1
-        
-        if(hDegree<=sDegree)count++
-        if(mDegree<=sDegree)count++
-        
-        count += h*120+2*m
-        count -=h
+    var answer = 0;
+
+    const getCount = (h,m,s)=>{
+        const hDegree = (h*30+ m*0.5 + s/120)%360
+        const mDegree = m*6 + s*0.1
+        const sDegree = 6*s
+        let count = 120*h -h +m*2 -1
+        if(sDegree >=mDegree){
+            count++
+        }
+        if(sDegree>=hDegree){
+            count++
+        }
         if(h>=12){
             count-=2
         }
-        return count
+
+        return count 
     }
-    answer = getCount(h2,m2,s2) - getCount(h1,m1,s1)
-    
-    if((h1===0 && m1===0 &&s1===0)||(h1===12&&m1===0&&s1===0)) answer++
-    
+
+    answer = getCount(h2,m2,s2)-getCount(h1,m1,s1)
+    if(m1==0 && s1==0 && (h1==0||h1==12)){
+        answer++
+    }
     return answer;
 }
