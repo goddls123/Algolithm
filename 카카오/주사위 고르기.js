d248388e1a9bec8123e558c5a2a5c4c31148a1ad
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-04-21 09:49:06 +0900

    [PO] 2024 WINTER INTERN Lv3 (5번)

--- 카카오/주사위 고르기.js
+++ 카카오/주사위 고르기.js
@@ -2,27 +2,25 @@ function solution(dice) {
     var answer = [];
     const n = dice.length
     const list = new Array(n).fill(0).map((a,i)=>i)
-    
-    const getComb =(L,array)=>{
-        if(L==1){
+    const getComb = (array,L)=>{
+        if(L===1){
             return array.map(a=>[a])
         }
         const result =[]
         array.forEach((fixed,index,origin)=>{
             const rest = origin.slice(index+1)
-            const comb = getComb(L-1,rest)
+            const comb = getComb(rest,L-1)
             const attach = comb.map(c=>[fixed,...c])
             result.push(...attach)
         })
-        
         return result
     }
-    const getList =(array,L)=>{
+    const getDiceList =(array,L)=>{
         if(L===0){
-            return dice[array[L]]
+            return dice[array[0]]
         }
-        const tmp = getList(array,L-1)
-        const result =[ ]
+        const tmp = getDiceList(array,L-1)
+        const result =[]
         for(let i=0;i<tmp.length;i++){
             for(let j=0;j<6;j++){
                 result.push(tmp[i]+dice[array[L]][j])
@@ -31,35 +29,36 @@ function solution(dice) {
         return result
     }
     
-    const binarySearch=(array,num)=>{
-        if(num>array[array.length-1]){
-            return array.length
-        }else if(num<=array[0]){
+    const binarySearch=(num,array)=>{
+        if(num<=array[0]){
             return 0
         }
-        let left = 0
+        if(num>array[array.length-1]){
+            return array.length
+        }
+        let left =0
         let right = array.length-1
         
         while(left<right){
             let mid = Math.floor((left+right)/2)
             
-            if(num>array[mid]){
+            if(array[mid]<num){
                 left = mid+1
             }else{
-                right = mid
+                right =mid
             }
         }
         return right
     }
     let max =0
-    getComb(n/2,list).forEach(dice1=>{
-        const dice2 = list.filter(l=>!dice1.includes(l))
-        const diceList1 = getList(dice1 , n/2-1)
-        const diceList2 = getList(dice2 , n/2-1).sort((a,b)=>a-b)
+    getComb(list,n/2).forEach(dice1=>{
+        const dice2 = list.filter(l=> !dice1.includes(l))
+        const dice1List = getDiceList(dice1,n/2-1)
+        const dice2List = getDiceList(dice2,n/2-1).sort((a,b)=>a-b)
         
-        let win = 0
-        for(let i=0;i<diceList1.length;i++){
-            win += binarySearch(diceList2,diceList1[i])    
+        let win=0
+        for(let i=0;i<dice1List.length;i++){
+            win += binarySearch(dice1List[i],dice2List)
         }
         if(max<win){
             max =win
