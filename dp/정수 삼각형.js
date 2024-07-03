a9395dfd0a1d5b8a18f2c07fc71e02a6a16024a7
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-03 11:15:40 +0900

    [PO] 정수 삼각형 Lv3 (3번)

--- dp/정수 삼각형.js
+++ dp/정수 삼각형.js
@@ -1,16 +1,15 @@
 function solution(triangle) {
     var answer = 0;
     const n = triangle.length
-    const dp = new Array(n+1)
-    for(let i=0;i<=n;i++){
-        dp[i]=new Array(i+2).fill(0)
-    }
-    dp[1][1] = triangle[0][0]
-    
-    
-    for(let i =2 ;i<=n;i++){
-        for(let j=1;j<=i;j++){
-            dp[i][j] = triangle[i-1][j-1] + Math.max(dp[i-1][j],dp[i-1][j-1])
+    const dp = Array.from(new Array(n+1), ()=>new Array(n+1).fill(0))
+ 
+    for(let i=1;i<=n;i++){
+        for(let j=0;j<i;j++){
+           if(j===0){
+               dp[i][j] = triangle[i-1][j] + dp[i-1][j]
+               continue
+           }
+            dp[i][j] = triangle[i-1][j] + Math.max(dp[i-1][j],dp[i-1][j-1])
         }
     }
     return Math.max(...dp[n]);
