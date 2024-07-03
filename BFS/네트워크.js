1eb175e2131ba2fa1e7ca0cae73e0269ce1437c8
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-03 12:53:56 +0900

    [PO] 네트워크 Lv3 (2번)

--- BFS/네트워크.js
+++ BFS/네트워크.js
@@ -2,26 +2,28 @@ function solution(n, computers) {
     var answer = 0;
     const visited = new Array(n).fill(false)
     
-    for(let i=0;i<n;i++){
-        if(!visited[i]){
-            BFS(i,visited,computers)
-            answer++
-        }
-    }
-    return answer;
-}
-const BFS = (start,visited,computers)=>{
-    const queue=[start]
-    visited[start] =true
+    const bfs = (start)=>{
+        const queue=[start]
+        visited[start]=true
     
-    while(queue.length){
-        const v =queue.shift()
-        
-        for(let i = 0; i<computers.length;i++){
-            if(!visited[i] && computers[v][i]){
-                visited[i]=true
-                queue.push(i)
+        while(queue.length){
+            const node = queue.shift()
+            
+            for(let i =0;i<n;i++){
+                if(i===node)continue
+                if(!visited[i] && computers[node][i]){
+                    visited[i]=true
+                    queue.push(i)
+                }
             }
         }
     }
+    
+   for(let i=0;i<n;i++){
+       if(!visited[i]){
+           bfs(i)
+           answer++
+       }
+   }
+    return answer;
 }
