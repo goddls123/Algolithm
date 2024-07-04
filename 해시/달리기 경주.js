4c20a292e8e43cfca3bc1e23103f72839a835265
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-04 13:03:56 +0900

    [PO] 달리기 경주 Lv2 (2번)

--- 해시/달리기 경주.js
+++ 해시/달리기 경주.js
@@ -1,16 +1,20 @@
 function solution(players, callings) {
     const map = new Map()
-    players.forEach((p,i)=>{
-        map.set(p,i)
-    })
-    callings.forEach(c=>{
-        const index = map.get(c)
-        const front = players[index-1]
-        map.set(c,index-1)
-        map.set(front,index)
-        players[index-1]=c
-        players[index]=front
+    for(let i=0;i<players.length;i++){
+        map.set(players[i],i)
+    }
+    
+    for(let name of callings){
+        const rank = map.get(name)
+        const name2 = players[rank-1]
         
-    })
-    return players
+        map.set(name, rank-1)
+        map.set(name2, rank)
+        
+        const tmp = players[rank]
+        players[rank] = players[rank-1]
+        players[rank-1 ]= tmp
+        
+    }
+    return players;
 }
