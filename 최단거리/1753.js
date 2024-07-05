42090206a216b85175f81c7f6425145d8c01a526
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-05 19:58:03 +0900

    [BOJ] 최단경로 골드4 (6번)

--- 최단거리/1753.js
+++ 최단거리/1753.js
@@ -2,14 +2,13 @@ class minHeap {
   constructor() {
     this.heap = [];
   }
-  swap(a, b) {
-    const tmp = this.heap[a];
-    this.heap[a] = this.heap[b];
-    this.heap[b] = tmp;
+  length() {
+    return this.heap.length;
   }
-  push(v) {
-    this.heap.push(v);
+  push(value) {
+    this.heap.push(value);
     if (this.heap.length === 1) return;
+
     let i = this.heap.length - 1;
     let parentI = Math.floor((i - 1) / 2);
 
@@ -20,76 +19,80 @@ class minHeap {
     }
   }
   pop() {
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
+    if (this.heap.length === 1) return this.heap.pop();
     const tmp = this.heap[0];
     this.heap[0] = this.heap.pop();
+
     let i = 0;
     let nextI = 0;
+
     while (1) {
       let leftI = i * 2 + 1;
       let rightI = i * 2 + 2;
       if (leftI >= this.heap.length) break;
-      if (this.heap[leftI][0] < this.heap[nextI][0]) {
+
+      if (this.heap[nextI][0] > this.heap[leftI][0]) {
         nextI = leftI;
       }
+
       if (
         rightI < this.heap.length &&
-        this.heap[rightI][0] < this.heap[nextI][0]
+        this.heap[nextI][0] > this.heap[rightI][0]
       ) {
         nextI = rightI;
       }
-      if (nextI === i) break;
-      this.swap(nextI, i);
+
+      if (i === nextI) break;
+
+      this.swap(i, nextI);
       i = nextI;
     }
+
     return tmp;
   }
-  getLength() {
-    return this.heap.length;
+  swap(a, b) {
+    const tmp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = tmp;
   }
 }
+
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
 const [N, M] = input.shift().split(" ").map(Number);
-const S = Number(input.shift());
+const start = Number(input.shift());
 
-const graph = Array.from(new Array(N + 1), () => []);
 const dp = new Array(N + 1).fill(Infinity);
+const graph = Array.from(new Array(N + 1), () => []);
+
 for (let i = 0; i < M; i++) {
   const [from, to, cost] = input[i].split(" ").map(Number);
-
   graph[from].push([cost, to]);
 }
-
 const dijkstra = (start) => {
   const queue = new minHeap();
+  dp[start] = 0;
   queue.push([0, start]);
-  while (queue.getLength() > 0) {
+
+  while (queue.length()) {
     const [cost, node] = queue.pop();
-
-    if (dp[node] < cost) continue;
-
-    dp[node] = cost;
+    if (cost > dp[node]) continue;
 
     for (let i = 0; i < graph[node].length; i++) {
-      const [nCost, next] = graph[node][i];
+      const [nextCost, next] = graph[node][i];
 
-      if (cost + nCost < dp[next]) {
-        dp[next] = cost + nCost;
+      if (nextCost + cost < dp[next]) {
+        dp[next] = nextCost + cost;
         queue.push([dp[next], next]);
       }
     }
   }
 };
 
-dijkstra(S);
-
+dijkstra(start);
 console.log(
   dp
     .slice(1)
-    .map((d) => (d === Infinity ? "INF" : d))
+    .map((n) => (n === Infinity ? "INF" : n))
     .join("\n")
 );
