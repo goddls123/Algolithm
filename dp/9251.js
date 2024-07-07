2317bf92df8447291e0637c61af71864c4378fc7
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-07 16:31:53 +0900

    [BOJ] LCS 골드5 (6번)

--- dp/9251.js
+++ dp/9251.js
@@ -1,22 +1,20 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
 const str1 = input[0];
 const str2 = input[1];
-
 const N = str1.length;
 const M = str2.length;
 
-const dp = Array.from(new Array(N + 1), () => new Array(M + 1).fill(0));
+const lcs = Array.from(new Array(N + 1), () => new Array(M + 1).fill(0));
 
 for (let i = 1; i <= N; i++) {
   for (let j = 1; j <= M; j++) {
     if (str1[i - 1] === str2[j - 1]) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
+      lcs[i][j] = lcs[i - 1][j - 1] + 1;
     } else {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
     }
   }
 }
 
-console.log(dp[N][M]);
+console.log(lcs[N][M]);
