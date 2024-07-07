dc2fb6a943883e160b06c69e112d128943aa0778
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-07 15:44:50 +0900

    [PO] 입국심사 Lv3 (3번)

--- 이분탐색/입국심사.js
+++ 이분탐색/입국심사.js
@@ -1,26 +1,21 @@
 function solution(n, times) {
-    var answer = 0;
-    let left =0
-    let right = 10**20
+    let right = 10**18;
+    let left = 1
     
-    const isPossible=(time)=>{
-        let count =0
-        for(let i=0;i<times.length;i++){
-            count+= Math.floor(time/times[i])
-        }
-        return n <= count
+    const isPossible=(mid)=>{
+        const number = times.reduce((accu,cur)=>
+                                    accu+Math.floor((mid/cur)),0)
+        return n<=number
     }
-    
-    let min = Infinity
-    while(left<=right){
+    while(left<right){
         let mid = Math.floor((left+right)/2)
         
         if(isPossible(mid)){
-            right = mid -1
-            min = Math.min(min,mid)
+            right = mid
         }else{
             left = mid+1
         }
     }
-    return min;
+    
+    return right;
 }
