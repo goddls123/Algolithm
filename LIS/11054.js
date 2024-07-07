9f8f9e24f6aec016d6ff8280adb7da13942379bc
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-07 16:03:34 +0900

    [BOJ] 가장 긴 바이토닉 부분 수열 골드4 (6번)

--- LIS/11054.js
+++ LIS/11054.js
@@ -1,35 +1,30 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
-const N = Number(input.shift());
-
-const nums = input[0].split(" ").map(Number);
+const N = Number(input[0]);
+const array = input[1].split(" ").map(Number);
 
 const increase = new Array(N).fill(1);
 const decrease = new Array(N).fill(1);
 
 for (let i = 1; i < N; i++) {
   for (let j = 0; j < i; j++) {
-    if (nums[i] > nums[j]) {
+    if (array[i] > array[j]) {
       increase[i] = Math.max(increase[i], increase[j] + 1);
     }
   }
 }
-
 for (let i = N - 2; i >= 0; i--) {
   for (let j = N - 1; j > i; j--) {
-    if (nums[i] > nums[j]) {
+    if (array[j] < array[i]) {
       decrease[i] = Math.max(decrease[i], decrease[j] + 1);
     }
   }
 }
-
-let max = 0;
+let count = 0;
 
 for (let i = 0; i < N; i++) {
-  if (max < increase[i] + decrease[i]) {
-    max = increase[i] + decrease[i];
+  if (decrease[i] + increase[i] > count) {
+    count = decrease[i] + increase[i];
   }
 }
-
-console.log(max - 1);
+console.log(count - 1);
