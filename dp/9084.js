8fe8aa54666c983568c2268282d85354a1e06fe7
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-08 18:31:27 +0900

    [BOJ] 동전 골드5 (4번)

--- dp/9084.js
+++ dp/9084.js
@@ -1,23 +1,21 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
 const T = Number(input.shift());
 
 const result = [];
-let i = 0;
-for (let k = 0; k < T; k++) {
-  const N = Number(input[i++]);
-  const coins = input[i++].split(" ").map(Number);
-  const money = Number(input[i++]);
-  const dp = new Array(money + 1).fill(0);
-  dp[0] = 1;
+for (let i = 0; i < T; i++) {
+  const N = Number(input.shift());
+  const coins = input.shift().split(" ").map(Number);
+  const K = Number(input.shift());
+  const money = new Array(K + 1).fill(0);
+  money[0] = 1;
   for (let j = 0; j < N; j++) {
-    for (let l = coins[j]; l <= money; l++) {
-      dp[l] = dp[l] + dp[l - coins[j]];
+    for (let k = 1; k <= K; k++) {
+      if (k < coins[j]) continue;
+      money[k] = money[k] + money[k - coins[j]];
     }
   }
-
-  result.push(dp[money]);
+  result.push(money[K]);
 }
 
 console.log(result.join("\n"));
