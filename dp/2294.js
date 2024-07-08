19d943764b0aec08f8b32f08de1db966d09fa2ba
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-08 16:51:45 +0900

    [BOJ] 동전 2 골드5 (6번)

--- dp/2294.js
+++ dp/2294.js
@@ -1,16 +1,15 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
 const [N, K] = input.shift().split(" ").map(Number);
-const coins = input.map(Number);
 
+const coins = input.map(Number);
 const money = new Array(K + 1).fill(Infinity);
 
 money[0] = 0;
 
 for (let i = 1; i <= K; i++) {
   for (let j = 0; j < N; j++) {
-    if (i - coins[j] < 0) continue;
+    if (coins[j] > i) continue;
     money[i] = Math.min(money[i], money[i - coins[j]] + 1);
   }
 }
