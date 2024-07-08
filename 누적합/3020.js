001353b03a2b811bdc5eb5df3bc7dd39a5c0e8d8
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-07-08 19:44:19 +0900

    [BOJ] 개똥벌레 골드5 (5번)

--- 누적합/3020.js
+++ 누적합/3020.js
@@ -1,35 +1,38 @@
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-
 const [N, H] = input.shift().split(" ").map(Number);
+
+const numbers = input.map(Number);
+
 const top = new Array(H + 1).fill(0);
 const bottom = new Array(H + 1).fill(0);
 
 for (let i = 0; i < N; i++) {
-  if (input[i] === 0) continue;
   if (i % 2 === 0) {
-    bottom[input[i]]++;
+    bottom[numbers[i]]++;
   } else {
-    top[H + 1 - input[i]]++;
+    top[H - numbers[i] + 1]++;
   }
+}
+
+for (let i = H - 1; i >= 1; i--) {
+  bottom[i] += bottom[i + 1];
 }
 
 for (let i = 2; i <= H; i++) {
   top[i] += top[i - 1];
 }
-for (let i = H - 1; i > 0; i--) {
-  bottom[i] += bottom[i + 1];
-}
 
 let min = Infinity;
 let count = 0;
+
 for (let i = 1; i <= H; i++) {
-  const c = top[i] + bottom[i];
-  if (min > c) {
-    min = c;
+  if (min > top[i] + bottom[i]) {
+    min = top[i] + bottom[i];
     count = 1;
-  } else if (min === c) {
+  } else if (min === top[i] + bottom[i]) {
     count++;
   }
 }
+
 console.log(min, count);
